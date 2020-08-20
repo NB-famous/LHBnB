d4e3d436436c0cc17dd2346f5d543dd67e36c164
Nikko B <ncbadoles@gmail.com>
2020-08-20 17:50:25 +0000

    modified reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -44,7 +44,7 @@ const getUserWithEmail = function(email) {
   SELECT email 
   FROM users
   WHERE email = $1
-  `, [email])
+  `,[email])
   .then(res => res.rows[0])
 };
 
@@ -104,9 +104,29 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+/* 
+Before query update
+
 const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
-}
+} */
+
+const getAllReservations = function(guest, limit = 10){
+
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest, limit])
+  .then(res => res.rows);
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
