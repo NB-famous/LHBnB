668c3244fa762a6cc167d61f0dafec2e8e119c70
Nikko B <ncbadoles@gmail.com>
2020-08-20 19:45:44 +0000

    Added Filters

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -148,11 +148,65 @@ const getAllProperties = function(options, limit = 10) {
 } 
 */
 
-const getAllProperties = function(options, limit = 10) {
+/* const getAllProperties = function(options, limit = 10) {
   return pool.query(`
   SELECT * FROM properties
   LIMIT $1
   `, [limit])
+  .then(res => res.rows);
+} */
+
+const getAllProperties = function(options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParmas.push(Number(options.owner_id));
+    queryString += `AND owner_id = $${queryParmas.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (queryParmas.length >= 1) queryString += 'AND ';
+    queryParmas.push(Number(options.minimum_price_per_night));
+    queryString += `cost_per_night >= $${queryParmas.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    if (queryParmas.length >= 1) queryString += 'AND ';
+    queryParmas.push(Number(options.maximum_price_per_night));
+    queryString += `cost_per_night >= $${queryParmas.length}`;
+  }
+
+  // 4
+  queryString += `GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParmas.push(Number(options.minimum_rating));
+    queryString += `HAVING avg(rating) >= $${queryParmas.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  //console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 
